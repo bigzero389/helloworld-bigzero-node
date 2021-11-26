30c9a57c12e21347465b1ff82222f318f734bdea
bigzero <dyheo@hist.co.kr>
2021-11-26 11:06:26 +0900

    add firehose

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 #FROM node:slim
 FROM 160270626841.dkr.ecr.ap-northeast-2.amazonaws.com/base/node:slim
+ENV NODE_ENV=production
+ENV HELLOWORLD_VERSION=1.0
 RUN mkdir -p /usr/local/HelloBigzeroWorld/
 COPY hello-bigzero-world.js package.json /usr/local/HelloBigzeroWorld/
 WORKDIR /usr/local/HelloBigzeroWorld/
 RUN npm install --production
 EXPOSE 3000
-ENV HELLOWORLD_VERSION 1.0
 ENTRYPOINT ["node", "hello-bigzero-world" ]
--- hello-bigzero-world.js
+++ hello-bigzero-world.js
@@ -1,11 +1,22 @@
 const HTTP = require("http")
-const WINSTON = require("winston")
-const WINSTON_DAILY = require("winston-daily-rotate-file")
 const AWS = require("aws-sdk")
-AWS.config.update({region:"ap-northeast-2"})
+AWS.config.update({region: "ap-northeast-2"})
+const WINSTON = require("winston")
 const WINSTON_CLOUDWATCH = require('winston-cloudwatch')
+const WINSTON_DAILY = require("winston-daily-rotate-file")
+const WINSTON_FIREHOSE = require("winston-firehose")
 
+const PORT = 3000
 const HELLOWORLD_VERSION = process.env.HELLOWORLD_VERSION
+const HOSTNAME = process.env.HOSTNAME
+
+const hostFormat = WINSTON.format(info => {
+    info.version = HELLOWORLD_VERSION
+    info.hostname = HOSTNAME
+    info.appname = "dy-helloworld"
+    return info
+})
+
 const OPTIONS = {
     file: {
         level: 'info',
@@ -44,29 +55,50 @@ const OPTIONS = {
         logStreamName: 'dy-helloworld',
         createLogGroup: true,
         createLogStream: true,
+    },
+    firehose: {
+        streamName: 'dy-kinesis-firehose-es',
+        region: 'ap-northeast-2'
     }
 };
 
-  const LOGGER = WINSTON.createLogger({
-    transports: [
-        new (WINSTON.transports.Console)(OPTIONS.console),
-        new WINSTON_CLOUDWATCH(OPTIONS.cloudwatch),
-        new WINSTON_DAILY(OPTIONS.file),
-        new WINSTON_DAILY(OPTIONS.errorFile),
-    ],
-    exitOnError: false,
-  });
+let LOGGER;
+
+if(process.env.NODE_ENV === 'production') {
+    LOGGER = WINSTON.createLogger({
+        transports: [
+            new WINSTON.transports.Console(OPTIONS.console),
+            new WINSTON_CLOUDWATCH(OPTIONS.cloudwatch),
+            new WINSTON_FIREHOSE(OPTIONS.firehose),
+            new WINSTON_DAILY(OPTIONS.file),
+            new WINSTON_DAILY(OPTIONS.errorFile),
+        ],
+        format: WINSTON.format.combine(
+            hostFormat(),
+            WINSTON.format.json()
+        ),
+        exitOnError: false,
+    });
+} else {
+    LOGGER = WINSTON.createLogger({
+        transports: [
+            new WINSTON.transports.Console(OPTIONS.console)
+        ],
+        exitOnError: false,
+    });
+}
+
 
 const CW_EVENTS = new AWS.CloudWatchEvents({apiVersion: "2015-10-07"})
-const CW = new AWS.CloudWatch({apiVersion:"2010-08-01"})
+const CW = new AWS.CloudWatch({apiVersion: "2010-08-01"})
 
 HTTP.createServer(function (request, response) {
     let event = {
-       Entries: [{
-           Detail: JSON.stringify(request.headers),
-           DetailType: "hello-bigzero-world application access request",
-           Source: "hello-bigzero-world.app"
-       }]
+        Entries: [{
+            Detail: JSON.stringify(request.headers),
+            DetailType: "hello-bigzero-world application access request",
+            Source: "hello-bigzero-world.app"
+        }]
     }
 
     let metric = {
@@ -83,22 +115,22 @@ HTTP.createServer(function (request, response) {
     }
 
     let isoDate = new Date().toISOString()
-    LOGGER.info("Hello bigzero world version "+ HELLOWORLD_VERSION +" is called at " + isoDate);
+    LOGGER.info("Hello bigzero world version " + HELLOWORLD_VERSION + " is called at " + isoDate);
     // Send the HTTP header
     // HTTP Status: 200 : OK
     // Content Type: text/plain
     response.writeHead(200, {'Content-Type': 'text/plain'})
     // Send the response body as "Hello World"
-    response.end('Hello bigzero world !!!, HELLOWORLD_VERSION:'+ HELLOWORLD_VERSION + ", DATE:" + isoDate +' \n')
+    response.end('Hello bigzero world !!!, HELLOWORLD_VERSION:' + HELLOWORLD_VERSION + ", DATE:" + isoDate + ' \n')
 
-    CW_EVENTS.putEvents(event, function(err, data){
+    CW_EVENTS.putEvents(event, function (err, data) {
         if (err) {
             LOGGER.error("error", "an error occurred when (createing an event", {error: err})
         } else {
             LOGGER.info("created event", {entries: data.Entries})
         }
     })
-    CW.putMetricData(metric, function(err, data) {
+    CW.putMetricData(metric, function (err, data) {
         if (err) {
             LOGGER.error("an error occurred when createing a metric", {error: err})
         } else {
@@ -109,4 +141,4 @@ HTTP.createServer(function (request, response) {
 
 // Console will print the message
 //console.log('Server running')
-LOGGER.info("Server is Running ==> port:3000, version:" + HELLOWORLD_VERSION)
+LOGGER.info("Server is Running ==> env:"+process.env.NODE_ENV+", hostname:"+HOSTNAME+", port:"+PORT+", version:"+HELLOWORLD_VERSION)
